1a71e29e8556e397cedf8c8e89d1d6bc8ffd2bef
ANUKAVALEDURGA <117466691+ANUKAVALEDURGA@users.noreply.github.com>
2023-01-25 14:26:20 +0530

    Update variables.tf

--- project3/variables.tf
+++ project3/variables.tf
@@ -1,10 +1,10 @@
 variable "bucket" {
-    default = ""
+    default = "my-s3-bucket-backend"
 }
 variable "hash_key" {
-    default = ""
+    default = "LockID"
 }
 
 variable "name" {
-    default = ""
+    default = "my_dynamo_db_backend_table"
 }
