730d9975145740574b8424016d99612a290fe476
ANUKAVALEDURGA <117466691+ANUKAVALEDURGA@users.noreply.github.com>
2023-01-25 14:19:49 +0530

    Update main.tf

--- project3/main.tf
+++ project3/main.tf
@@ -1,1 +1,10 @@
+module "aws_s3_create" {
+    source = "./modules/s3_bucket"
+    bucket = var.bucket
+}
 
+module "dynamo_db_create" {
+    source = "./modules/DynamoDB"
+    hash_key = var.hash_key
+    name = var.name
+}
