510a3b852b4f3c00720d9be21fec900b178b0b11
ANUKAVALEDURGA <117466691+ANUKAVALEDURGA@users.noreply.github.com>
2023-01-25 13:30:33 +0530

    Update main.tf

--- project3/mdoules/DynamoDB/main.tf
+++ project3/mdoules/DynamoDB/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = var.dynamodb_name
+  name           = var.name
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = var.hash_key
   attribute {
