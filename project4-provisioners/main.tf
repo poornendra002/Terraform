61f03f567c70f890267ae29a4684baf28a2d3b88
ANUKAVALEDURGA <117466691+ANUKAVALEDURGA@users.noreply.github.com>
2023-01-26 17:03:35 +0530

    Create main.tf

---
+++ project4-provisioners/main.tf
@@ -0,0 +1,23 @@
+module "ec2_create" {
+    source = "./modules/ec2"
+    ec2_ami = "ami-0597375488017747e"
+    ec2_instance_type = "t2.micro"
+    ec2_pem = "devopslearning"
+}
+
+module "ec2_file_copy_provisioner" {
+    source = "./modules/file"
+    ec2_user = "ubuntu"
+    ec2_public_ip = module.ec2_create.ec2_public_ip
+}
+
+module "ec2_remote_exec_provisioner" {
+    source = "./modules/remote-exec"
+    ec2_user = "ubuntu"
+    ec2_public_ip = module.ec2_create.ec2_public_ip
+}
+
+module "ec2_local_exec_provisioner" {
+    source = "./modules/local-exec"
+    ec2_public_ip = module.ec2_create.ec2_public_ip
+}
